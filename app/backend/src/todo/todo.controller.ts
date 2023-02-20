0e2f63b841e099b99a151cbc7c02dd50b28ca7e1
wagatsuma.yuichi <firefly.of.april@gmail.com>
2023-02-20 20:16:46 +0900

    [add] 13.Todo Service ++ Controller

--- app/backend/src/todo/todo.controller.ts
+++ app/backend/src/todo/todo.controller.ts
@@ -1,4 +1,69 @@
-import { Controller } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+  Req,
+} from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
+import { Request } from 'express'
+import { TodoService } from './todo.service'
+import { CreateTaskDto } from './dto/create-task.dto'
+import { UpdateTaskDto } from './dto/update-task.dto'
+import { Task } from '@prisma/client'
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Get()
+  async getTasks(@Req() req: Request): Promise<Task[]> {
+    // strategyによるミドルウェアのvalidationメソッドの返り値であるuserオブジェクトは自動的にHTTPリクエストに含まれるため、req.user.idでユーザーIDが引数として渡せる
+    return await this.todoService.getTasks(req.user.id)
+  }
+
+  @Get(':id')
+  async getTaskById(
+    @Req() req: Request,
+    // ParseIntPipeを引数に指定することでreq.user.idをデフォルトの文字列型から数値型に変換する
+    @Param('id', ParseIntPipe) taskId: number,
+  ): Promise<Task> {
+    return await this.todoService.getTaskById(req.user.id, taskId)
+  }
+
+  @Post()
+  async createTask(
+    @Req() req: Request,
+    @Body() dto: CreateTaskDto,
+  ): Promise<Task> {
+    return await this.todoService.createTask(req.user.id, dto)
+  }
+
+  /** 単一のタスクを更新する関数オブジェクト */
+  @Patch(':id')
+  async updateTaskById(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) taskId: number,
+    @Body() dto: UpdateTaskDto,
+  ): Promise<Task> {
+    return await this.todoService.updateTaskById(req.user.id, taskId, dto)
+  }
+
+  /** 単一のタスクを削除する関数オブジェクト */
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  async deleteTaskById(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) taskId: number,
+  ): Promise<void> {
+    return await this.todoService.deleteTaskById(req.user.id, taskId)
+  }
+}
--- app/backend/src/todo/todo.service.ts
+++ app/backend/src/todo/todo.service.ts
@@ -1,4 +1,88 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, ForbiddenException } from '@nestjs/common'
+import { PrismaService } from '../prisma/prisma.service'
+import { CreateTaskDto } from './dto/create-task.dto'
+import { UpdateTaskDto } from './dto/update-task.dto'
+import { Task } from '@prisma/client'
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  /**
+   * ユーザーが作成したタスクだけを全て取得するメソッド
+   */
+  async getTasks(userId: number): Promise<Task[]> {
+    return await this.prisma.task.findMany({
+      where: {
+        userId,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+  }
+  /**
+   * ユーザーが作成したタスクを一つだけ取得するメソッド
+   */
+  async getTaskById(userId: number, taskId: number): Promise<Task> {
+    return await this.prisma.task.findFirst({
+      where: {
+        userId,
+        id: taskId,
+      },
+    })
+  }
+  /**
+   * タスクを一つ作成するメソッド
+   */
+  async createTask(userId: number, dto: CreateTaskDto): Promise<Task> {
+    return await this.prisma.task.create({
+      data: {
+        userId,
+        ...dto,
+      },
+    })
+  }
+
+  /*  */
+  async updateTaskById(
+    userId: number,
+    taskId: number,
+    dto: UpdateTaskDto,
+  ): Promise<Task> {
+    const task = await this.prisma.task.findUnique({
+      where: {
+        id: taskId,
+      },
+    })
+
+    if (!task || task.userId !== userId)
+      throw new ForbiddenException('No permission to update')
+
+    return await this.prisma.task.update({
+      where: {
+        id: taskId,
+      },
+      data: {
+        ...dto,
+      },
+    })
+  }
+
+  async deleteTaskById(userId: number, taskId: number): Promise<void> {
+    const task = await this.prisma.task.findUnique({
+      where: {
+        id: taskId,
+      },
+    })
+
+    if (!task || task.userId !== userId)
+      throw new ForbiddenException('No permission to delete')
+
+    await this.prisma.task.delete({
+      where: {
+        id: taskId,
+      },
+    })
+  }
+}
